be1700bac4f5380c25d33222337917777fbb9ba7
Gabriel <starktechmg@outlook.com>
2024-03-11 22:07:10 -0300

    Descrição de adição

---
+++ asyncio.py
@@ -0,0 +1,30 @@
+"""Async usage
+Simply import AsyncOpenAI instead of OpenAI and use await with each API call: """
+
+
+
+import os
+import asyncio
+from openai import AsyncOpenAI
+
+client = AsyncOpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+
+async def main() -> None:
+    chat_completion = await client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "Say this is a test",
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+
+
+asyncio.run(main())
+
+
