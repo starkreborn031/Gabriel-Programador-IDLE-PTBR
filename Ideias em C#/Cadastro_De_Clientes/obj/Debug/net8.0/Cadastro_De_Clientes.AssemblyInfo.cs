7d3c65ce76bab086124a345d86afae899fa765f7
starkreborn031 <starktechmg@outlook.com>
2024-11-05 21:57:28 -0300

    Noite 2

--- Ideias em C#/Cadastro_De_Clientes/obj/Debug/net8.0/Cadastro_De_Clientes.AssemblyInfo.cs
+++ Ideias em C#/Cadastro_De_Clientes/obj/Debug/net8.0/Cadastro_De_Clientes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cadastro_De_Clientes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e2d50f4dd615407169387a3e8b89180c41892b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef14bcf36d9a6710aa53ef0e403f5047929e38c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cadastro_De_Clientes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cadastro_De_Clientes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
